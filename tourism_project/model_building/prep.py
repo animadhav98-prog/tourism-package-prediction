97f0dc37f81ee1ee3fc353a03f109412731adfc8
animadhav98-prog <ani.madhav98@gmail.com>
2026-01-29 16:54:27 +0000

    Fixed index issue in prep.py

--- tourism_project/model_building/prep.py
+++ tourism_project/model_building/prep.py
@@ -55,10 +55,10 @@ y = tourism_dataset[target]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2,
                                                     random_state = 42)
 
-X_train.to_csv("Xtrain.csv")
-X_test.to_csv("Xtest.csv")
-y_train.to_csv("ytrain.csv")
-y_test.to_csv("ytest.csv")
+X_train.to_csv("Xtrain.csv",index=False)
+X_test.to_csv("Xtest.csv",index=False)
+y_train.to_csv("ytrain.csv",index=False)
+y_test.to_csv("ytest.csv",index=False)
 
 files = ["Xtrain.csv","Xtest.csv","ytrain.csv","ytest.csv"]
 
